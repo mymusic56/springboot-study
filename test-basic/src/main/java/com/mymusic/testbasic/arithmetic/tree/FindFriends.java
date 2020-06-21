2ab98c7129961eb5b1121e12d447ad2c31c721c2
zhangshengji <961272668@qq.com>
2020-06-21 23:13:37 +0800

    'search_friends'

--- test-basic/src/main/java/com/mymusic/testbasic/arithmetic/tree/FindFriends.java
+++ test-basic/src/main/java/com/mymusic/testbasic/arithmetic/tree/FindFriends.java
@@ -14,24 +14,18 @@ public class FindFriends {
         //生成20个用户， 每个用户有2-5个好友
         int size = 20;
         People people = new People(size);
-        int[] a = new int[5];
-        a[0] = 1;
-        a[1] = 1;
-        a[2] = 1;
-        a[3] = 1;
-        System.out.println(Arrays.toString(a));
 
         //随机生成好友
-        for (Node node: people.user_nodes) {
+        for (Node node : people.user_nodes) {
             System.out.println("---------------");
-            int friends_count = (int)(Math.random()*5 + 2);
-            System.out.println("用户：" + node.user_id + ", 好友数量："+friends_count);
+            int friends_count = (int) (Math.random() * 5 + 2);
+            System.out.println("用户：" + node.user_id + ", 好友数量：" + friends_count);
             int[] friends_list = new int[friends_count];
             for (int i = 0; i < friends_count; i++) {
-                int friends_id = (int)(Math.random()*size + 1);
+                int friends_id = (int) (Math.random() * size + 1);
                 //判断是否有重复的好友
                 boolean is_repeat = false;
-                for (int friend: friends_list ) {
+                for (int friend : friends_list) {
                     if (friend == friends_id) {
                         is_repeat = true;
                         break;
@@ -43,7 +37,7 @@ public class FindFriends {
                 friends_list[i] = friends_id;
                 node.friends.add(friends_id);
             }
-            System.out.println("用户：" + node.user_id + "好友："+Arrays.toString(node.friends.toArray()));
+            System.out.println("用户：" + node.user_id + "好友：" + Arrays.toString(node.friends.toArray()));
             System.out.println("---------------");
         }
 
@@ -58,52 +52,65 @@ class People {
 
     public People(int size) {
         this.user_nodes = new Node[size];
-        for(int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 20; i++) {
             add(new Node(i));
         }
     }
 
     public void add(Node node) {
-        this.user_nodes[node.user_id-1] = node;
+        this.user_nodes[node.user_id - 1] = node;
     }
 
     /**
      * 找出互为二度好友的用户
-     * 还未完成？？？？？
+     *
      * @param user_id
      */
     public void bfs(int user_id) {
-        System.out.println("----------查找用户: "+user_id+" 的好友-------------");
-        Node node = this.user_nodes[user_id-1];
+        System.out.println("----------查找用户: " + user_id + " 的好友-------------");
+        //克隆一份数据
+        Node[] copy = this.user_nodes.clone();
+        Node node = copy[user_id - 1];
         if (node == null) {
-            System.out.println("用户："+user_id+"不存在");
+            System.out.println("用户：" + user_id + "不存在");
             System.exit(0);
         }
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.offer(user_id);
         HashSet<Integer> visited = new HashSet<>();
-        int degree = 1;
+        HashMap<Integer, HashSet<Integer>> relation = new HashMap<>();
+        int degree = 0;
+        HashSet<Integer> tmp = null;
         while (!queue.isEmpty()) {
-            int current_user_id = ((LinkedList<Integer>) queue).pop();
-            if (visited.contains(current_user_id)) {
-                continue;
-            }
-            Node friends = this.user_nodes[current_user_id-1];
-            HashSet<Integer> tmp_degree_friends = new HashSet<>();
-            for (int friend_id: friends.friends) {
+            int current_user_id = queue.poll();
+            Node friends = copy[current_user_id - 1];
+            for (int friend_id : friends.friends) {
+                if (visited.contains(friend_id)) {
+                    continue;
+                }
                 if (friend_id == current_user_id) {
                     //好友是待搜索人自己，跳过
                     continue;
                 }
-                tmp_degree_friends.add(friend_id);
-                ((LinkedList<Integer>) queue).add(friend_id);
-            }
-            visited.add(current_user_id);
+                degree = friends.degree + 1;
+                tmp = new HashSet<>();
+                if (relation.containsKey(degree)) {
+                    tmp = relation.get(degree);
+                }
+                tmp.add(friend_id);
+                visited.add(friend_id);
 
-            //获取几度好友有问题
-            System.out.println(degree + "度好友： "+Arrays.toString(tmp_degree_friends.toArray()));
+                relation.put(degree, tmp);
+                copy[friend_id - 1].degree = friends.degree + 1;
+                queue.offer(friend_id);
+            }
         }
 
+        for (Map.Entry<Integer, HashSet<Integer>> entry : relation.entrySet()) {
+            int key = entry.getKey();
+            HashSet<Integer> friends = entry.getValue();
+            System.out.println(key + "度好友： " + Arrays.toString(friends.toArray()));
+        }
     }
 }
 
